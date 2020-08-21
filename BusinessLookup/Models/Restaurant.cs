510f22d9f3f15003f58a1b52655055da24122987
Jeff Kim <kim2jy@hotmail.com>
2020-08-21 09:29:26 -0700

    Add Restaurant model, fix appsettings and startup file errors

---
+++ BusinessLookup/Models/Restaurant.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BusinessLookup.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Cuisine { get; set; }
+    public string PhoneNumber { get; set; }
+  }
+}
