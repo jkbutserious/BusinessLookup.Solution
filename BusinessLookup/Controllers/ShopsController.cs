240c034160b05bd05766ff4446c5b01a4b0d3d06
Jeff Kim <kim2jy@hotmail.com>
2020-08-21 09:59:23 -0700

    Add Shops controller with queryable API get route

---
+++ BusinessLookup/Controllers/ShopsController.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLookup.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLookup.Controllers
+{
+  [Route("api/[controller")]
+  [ApiController]
+  public class ShopsController : ControllerBase
+  {
+    private BusinessLookupContext _db;
+
+    public ShopsController(BusinessLookupContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/shops
+    [HttpGet]
+    public ActionResult<IEnumerable<Shop>> Get(int? id, string name, string type, int? phone)
+    {
+      var query = _db.Shops.AsQueryable();
+
+      if (id != null)
+      {
+        query = query.Where(entry => entry.ShopId == id);
+      }
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if (type != null)
+      {
+        query = query.Where(entry => entry.Type == type);
+      }
+      if (phone != null)
+      {
+        query = query.Where(entry => entry.PhoneNumber == phone);
+      }
+
+      return query.ToList();
+    }
+
+    // POST api/shops
+    [HttpPost]
+    public void Post([FromBody] Shop shop)
+    {
+      _db.Shops.Add(shop);
+      _db.SaveChanges();
+    }
+
+    // PUT api/shops/[ShopId]
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Shop shop)
+    {
+      shop.ShopId = id;
+      _db.Entry(shop).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/shops/[MessageId]
+    public void Delete(int id)
+    {
+      var shopToRemove = _db.Shops.FirstOrDefault(entry => entry.ShopId == id);
+      _db.Shops.Remove(shopToRemove);
+      _db.SaveChanges();
+    }
+  }
+}
--- BusinessLookup/Models/Shop.cs
+++ BusinessLookup/Models/Shop.cs
@@ -7,6 +7,6 @@ namespace BusinessLookup.Models
     public int ShopId { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
-    public string PhoneNumber { get; set; }
+    public int PhoneNumber { get; set; }
   }
 }
