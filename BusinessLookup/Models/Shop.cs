c4fe4b9a03407d05af0f07ff38a08ee08426a697
Jeff Kim <kim2jy@hotmail.com>
2020-08-21 09:27:38 -0700

    Add Shop model

---
+++ BusinessLookup/Models/Shop.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BusinessLookup.Models
+{
+  public class Shop
+  {
+    public int ShopId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string PhoneNumber { get; set; }
+  }
+}
